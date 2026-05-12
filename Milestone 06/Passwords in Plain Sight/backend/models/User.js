440b365640ca8ecdfc68d548101a8cc3cfe1de19
Jeeva Niteesh <jeevanithish2007@gmail.com>
2026-05-12 10:21:56 +0530

    Fix insecure password storage and comparison

--- Milestone 06/Passwords in Plain Sight/backend/models/User.js
+++ Milestone 06/Passwords in Plain Sight/backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,10 +12,24 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      // No minlength, no select: false, no pre-save hook
+      minlength: 8,
+      select: false,
     },
   },
   { timestamps: true }
 )
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  this.password = await bcrypt.hash(this.password, 12)
+  next()
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password)
+}
 
 export default mongoose.model('User', userSchema)
